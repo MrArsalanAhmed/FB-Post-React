a1f88de761c5427daeda8d74f90cd4f148906db8
Arsalan Ahmed <arsal.ahmed992@gmail.com>
2023-08-23 06:53:28 +0500

    Facebook Post clone

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,80 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import "./index.css";
+// import '@fortawesome/fontawesome-free/css/all.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function Card(props) {
+  return (
+    <div className="facebookCard">
+      <div className="cardHeader">
+        <img className="profileImg" src={props.profileImgUrl} alt="profileImage" />
+        <div className="userInfo">
+          <h3>{props.name}</h3>
+          <span className="postDate">{props.postDate}</span>
+        </div>
+      </div>
+      <p>{props.text}</p>
+      <img
+        className="postImage"
+        src={props.imgUrl}
+        alt="Post"
+        width="500"
+        height="300"
+      />
+
+      <div className="postButtons">
+        <button>Like</button>
+        <button>Comment</button>
+        <button>Share</button>
+      </div>
+    </div>
+  );
+}
+
+const postsData = [
+  {
+    postDate: "25 August 2023",
+    profileImgUrl: "https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg",
+    imgUrl: "https://move2turkey.com/wp-content/uploads/2021/06/Temple-of-Artemis-in-Turkey-One-of-the-seven-wonders-of-the-ancient-world3.jpg",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto natus corrupti perspiciatis totam! Cumque ab eum sunt error vero, recusandae doloribus, delectus dignissimos nobis molestias ex nihil officia assumenda maiores.",
+    name: "Ahmed",
+  },
+  {
+    postDate: "15 March 2023",
+    profileImgUrl: "https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg",
+    imgUrl: "https://nomadparadise.com/wp-content/uploads/2021/04/pakistan-places-07.jpg",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto natus corrupti perspiciatis totam! Cumque ab eum sunt error vero, recusandae doloribus, delectus dignissimos nobis molestias ex nihil officia assumenda maiores.",
+    name: "Arsalan",
+  },
+  {
+    postDate: "30 December 2022",
+    profileImgUrl: "https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg",
+    imgUrl: "https://www.forbesindia.com/media/images/2023/May/img_207569_thomasedison.jpg",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto natus corrupti perspiciatis totam! Cumque ab eum sunt error vero, recusandae doloribus, delectus dignissimos nobis molestias ex nihil officia assumenda maiores.",
+    name: "Dave",
+  }, 
+  {
+    postDate: "31 July 2022",
+    profileImgUrl: "https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg",
+    imgUrl: "https://images.unsplash.com/photo-1570294646112-27ce4f174e38?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bGFtYm9yZ2hpbml8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto natus corrupti perspiciatis totam! Cumque ab eum sunt error vero, recusandae doloribus, delectus dignissimos nobis molestias ex nihil officia assumenda maiores.",
+    name: "John",
+  },
+  // Add more posts here
+];
+
+ReactDOM.render(
+  <div>
+    {postsData.map((post, index) => (
+      <Card
+        key={index}
+        postDate={post.postDate}
+        profileImgUrl={post.profileImgUrl}
+        imgUrl={post.imgUrl}
+        text={post.text}
+        name={post.name}
+      />
+    ))}
+  </div>,
+  document.querySelector("#root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
